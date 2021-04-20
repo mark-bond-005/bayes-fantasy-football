6d0aa6b8f66af1c1110c6906f8afae95737b6365
Mark Bond <36740324+mark-bond-005@users.noreply.github.com>
2021-04-20 16:25:05 -0500

    Add files via upload

--- ff.R
+++ ff.R
@@ -1,8 +1,9 @@
+library(reshape2)
+
 # Try to distribute tau as gamma (0.00001, 0.00001)
 
-
+setwd("C:/Users/mab7986/Desktop/bayes_fantasy_football-master")
 nWeeks <- 13
-#setwd("C:/Users/Mark Bond/Documents/Fantasy Football")
 # Read the data in, make some obvious modifications
 ff <- read.csv("ff.csv", stringsAsFactors = FALSE)
 ff$actualWin <- ifelse(ff$actualOne > ff$actualTwo, 1, 2)
@@ -24,20 +25,23 @@ awfulLm <- lm(team$actual ~ team$proj)
 plot(team$proj, team$actual, xlab="Projected Team Scores", ylab = "Actual Team Scores")
 curve(coef(awfulLm)[1] +coef(awfulLm)[2]*x, add = TRUE)
 
-library(reshape)
+# Finally, we begin the Bayesian analysis.
+library(R2WinBUGS)
 
-# We create two separate long datasets for this, the "projected" dataset and the "actual" dataset
-eZ <- team[ , 1:3]
-eZ$variable <- paste( "proj", eZ$week, sep = "")
-eZ$value <- eZ$proj
-wide <- cast(eZ, ID ~ variable)
-
-eZ2 <- team[ , -3]
-eZ2$variable <- paste( "actual", eZ2$week, sep = "")
-eZ2$value <- eZ2$actual
-wide2 <- cast(eZ2, ID ~ variable)
-#Now we merge these two datasets
-ffWide <- merge(wide, wide2, by = "ID")
+teamId <- as.numeric(as.factor(team$ID))
+actual <- team$actual
+zProj <- team$zProj
+J <- length(unique(ID))
+n <- length(team$actual)
+# This data list calls the variables named in the previous 5 lines.
+footData <- list("n", "actual", "zProj")
+footInits <- function(){
+  list(intercept = rnorm(1), b = rnorm(1),
+       sigmaY = runif(1))
+}
+footParm <- c("intercept", "b", "sigmaY")
+footBayes_simple <- bugs(footData, footInits, footParm, "football.bug", n.chains = 3, n.iter=500, n.burnin=100, debug=TRUE)
+print(footBayes_simple)
 
 # Here is a frequentist HLM.
 library(arm)
@@ -45,42 +49,29 @@ freqLm <- lmer(actual ~ zProj + (1 | ID), data = team)
 summary(freqLm)
 skill <- ranef(freqLm)
 
-# Finally, we begin the Bayesian analysis.
-library(R2WinBUGS)
-teamId <- as.numeric(team$ID)
-actual <- team$actual
-zProj <- team$zProj
-J <- length(unique(ID))
-n <- length(team$actual)
-# This data list calls the variables named in the previous 5 lines.
-footData <- list("n", "J", "actual", "teamId", "zProj")
+footData2 <- list("n", "J", "actual", "teamId", "zProj")
 # Initial values
-footInits <- function() {
+footInits2 <- function() {
   list(a = rnorm(J), b = rnorm(1), muA = rnorm(1),
        sigmaY = runif(1), sigmaA = runif(1))
   }
 #Names the parameters we're interested in.
-footParm <-c("a", "b", "muA", "sigmaY", "sigmaA", "varPctA", "varPctY")
+footParm2 <-c("a", "b", "muA", "sigmaY", "sigmaA", "varPctA", "varPctY")
 
 # In addition to the lists and vectors defined above, we use "football.bug", a file we wrote with notepad. Go read it!
-footBayes <- bugs(footData, footInits, footParm, "football3.bug", n.chains = 3, n.iter=10000, n.burnin=6000)
+footBayes <- bugs(footData2, footInits2, footParm2, "football2.bug", n.chains = 3, n.iter=500, n.burnin=100, debug=TRUE)
+print(footBayes)
 attach.bugs(footBayes)
 aMultilvl <- rep(NA, J)
 for(j in 1:J){ aMultilvl[j] <- median(a[ , j])}
 bMultilvl <- median(b)
 
-team$ability <- rep(aMultilvl, 6)
-
-for(i in 1:length(teamNames)) {
-  ff[ ff$teamOne == teamNames[i], "abilityOne"] <- aMultilvl[i]
-  ff[ ff$teamTwo == teamNames[i], "abilityTwo"] <- aMultilvl[i]
-}
-
-ff$actualWin <- -1*ff$actualWin + 2 # Now a One indicates that teamOne wins, and a 0 indicates that teamTwo wins.
-ff$abilDif <- ff$abilityOne - ff$abilityTwo
-freqLogLm <- glm( actualWin ~ abilDif, data = ff, family = binomial)
-plot(ff$actualWin ~ ff$abilDif, xlab = "Projected point spread", ylab = "Whether team one wins")
-lines(ff$abilDif, freqLogLm$fitted, type="l", col="red")
-
-qplot(team$actual, team$proj)
-#fully Bayesian fixed-slope varying-intercept hierarchical linear model
+footData3 <- footData2
+footInits3 <- function() {
+  list(a = rnorm(J), b = rnorm(J), muA = rnorm(1),
+       sigmaY = runif(1), sigmaA = runif(1), muB =rnorm(1), sigmaB = runif(1))
+  }
+#Names the parameters we're interested in.
+footParm3 <-c("a", "b", "muA", "muB", "sigmaY", "sigmaA", "varPctA", "varPctY")
+footBayes_complex <- bugs(footData3, footInits3, footParm3, "football3.bug", n.chains = 3, n.iter=2000, n.burnin=500, debug=TRUE)
+print(footBayes_complex)
